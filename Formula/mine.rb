0f0d68ff6675af509bc20fdf922482de6eaf26b6
Joel Zandén <info@joelzanden.se>
2023-03-21 12:22:43 +0100

    Update to v0.1.19

--- Formula/mine.rb
+++ Formula/mine.rb
@@ -1,16 +1,14 @@
 class Mine < Formula
   desc "CLI for color-coding project windows, enhancing workspace organization"
   homepage "https://github.com/joelzanden/mine"
-  url "https://github.com/joelzanden/mine/archive/refs/tags/v0.1.17.tar.gz"
-  sha256 "918dc80546019e9c1573c6d133388eee806c0a034afce3d496a165c6279cf597"
+  url "https://github.com/joelzanden/mine/archive/refs/tags/v0.1.19.tar.gz"
+  sha256 "da488069d39339868cc00428b517cb96fb8c7d339b26522d5859e33373134d42"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/joelzanden/mine/releases/download/v0.1.17"
-    sha256 catalina:      "1b25ab24287076f7f19909374aa7d7808887dcb09771250d5d666da6154592fd"
-    sha256 big_sur:       "dd6d24a75b2b2819f06a622b8620836ae57a142d889dda33501d4632c2e032e3"
-    sha256 monterey:      "2575096dfdac6c3c30d6fb1153e0f3aa9ffe89e8b3cf576329cbc089e0f8d325"
-    sha256 x86_64_linux:  "2b43c1abde40a05c8738de0f46ace477dfa0666717af1c91af39ef61d2933f98"
+    root_url "https://github.com/joelzanden/mine/releases/download/v0.1.19"
+    sha256 catalina:      "97b6e3f205f17c788b870bd30849ede9de67fb3eb68f7529e33f884a30049359"
+    sha256 x86_64_linux:  "f2e26d2f9372db01c6f022c69799f9ea05fcafa72c96d26f82e7ae975c770e50"
   end
 
   depends_on "rust" => :build
@@ -20,6 +18,6 @@ class Mine < Formula
   end
 
   test do
-    assert_match "0.1.17", shell_output("#{bin}/mine --version")
+    assert_match "0.1.19", shell_output("#{bin}/mine --version")
   end
 end
