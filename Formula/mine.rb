696ad7f11bb49619a55e9487fdf4fcafb1bcf08f
joelzanden <joelzanden@users.noreply.github.com>
2023-06-12 08:32:18 +0000

    Update mine.rb

--- Formula/mine.rb
+++ Formula/mine.rb
@@ -6,7 +6,7 @@ class Mine < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/joelzanden/mine/releases/download/v0.1.23"
+    root_url "https://github.com/joelzanden/mine/releases/download/v0.1.24"
     sha256 monterey:     "f9dffeb28fa3ff121674f33a9356679d9c2616334833370ffb453252bee2a198"
     sha256 x86_64_linux: "8b1c2c0e278cb726bf51c119c74b8ba1b436520a190baeb8f1de72398b9fb2ef"
   end
@@ -18,6 +18,6 @@ class Mine < Formula
   end
 
   test do
-    assert_match "0.1.23", shell_output("#{bin}/mine --version")
+    assert_match "0.1.24", shell_output("#{bin}/mine --version")
   end
 end
