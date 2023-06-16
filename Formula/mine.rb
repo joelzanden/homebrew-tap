953765a97b38b5014ffb5911cc5c147907b1080f
joelzanden <joelzanden@users.noreply.github.com>
2023-06-16 06:55:57 +0000

    Update mine.rb

--- Formula/mine.rb
+++ Formula/mine.rb
@@ -5,12 +5,6 @@ class Mine < Formula
   sha256 "07ef6ba2aaf48ac2ff369b34a624caef62448e9d03b839d8a575c50180304bca"
   license "MIT"
 
-  bottle do
-    root_url "https://github.com/joelzanden/mine/releases/download/0.1.25"
-    sha256 monterey:     "2d6373074b57cb6fe5cce086251226af5d185613dd311c83598e13b01ad35b3a"
-    sha256 x86_64_linux: "a28a1c42a7fe520882125e0dc56bbc5475f7902f34f3100382b515b1fec35b54"
-  end
-
   depends_on "rust" => :build
 
   def install
