2e74184758d6286cec9ded1dfc38b6a7346eecec
Joel Zandén <info@joelzanden.se>
2025-01-21 09:19:52 +0100

    Update send.rb

--- Formula/send.rb
+++ Formula/send.rb
@@ -1,6 +1,5 @@
 class Send < Formula
-  desc "A script to upload files to dropdrop.download. It returns a link to the uploaded file. Current limit is 100MB and files expire after 4 days."
-  version "0.0.6"
+  desc "Upload files to dropdrop.download"
   homepage "https://github.com/joelzanden/send"
   url "https://github.com/joelzanden/send/releases/download/v0.0.6/send-0.0.6.tar.gz"
   sha256 "7ebe3b5b2667b4df78f20aad919a40b93234bf860ff9c2eec42541ca0e693048"
