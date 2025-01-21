965a2d3a0cfd109f0c29aeff3a9b2d31a33fa4fc
Joel Zandén <info@joelzanden.se>
2025-01-21 09:19:54 +0100

    Update rebun.rb

--- Formula/rebun.rb
+++ Formula/rebun.rb
@@ -1,12 +1,11 @@
 class Rebun < Formula
   desc "Like bun run but with delayed restart after crash"
-  version "0.4.0"
   homepage "https://github.com/joelzanden/rebun"
   url "https://github.com/joelzanden/rebun/releases/download/v0.4.0/rebun-0.4.0.tar.gz"
   sha256 "13f61468931f95d51f0e9ded161a0ea3523f05b30e98346edac22e627544e030"
   license "MIT"
 
-  depends_on "bun"
+  depends_on "oven-sh/bun/bun"
 
   def install
     bin.install "rebun"
